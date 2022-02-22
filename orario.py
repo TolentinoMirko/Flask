4d922b95eaa1d9748770889a5cc194ab8a456fd3
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2022-02-22 21:55:06 +0000

    Orario senza colori

--- orario.py
+++ orario.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template,request
-import time
+from flask import Flask, render_template
 app = Flask(__name__)
-
+from datetime import datetime
+now = datetime.now()
 @app.route('/', methods=['GET'])
-def hello_world():
-    return render_template("indexOrario.html")
-
-@app.route('/GetTime', methods=['GET'])
-def GetTime():
-    print('browser time: ', request.args.get("time"))
-    print('server time:' time.strftime('%A %B, %d %Y %H:%M:%S'));
-    return "Done")
-
+def time():
+  now = datetime.now()
+  if now.hour < 7:
+        return render_template("indexOrario.html", time=f"{now.hour}:{now.minute}:{now.second}", color="cyan")
+  elif now.hour < 13:
+      return render_template("indexOrario.html", time=f"{now.hour}:{now.minute}:{now.second}", color="yellow")
+  elif now.hour < 18:
+      return render_template("indexOrario.html", time=f"{now.hour}:{now.minute}:{now.second}", color="orange")
+  else:
+      return render_template("indexOrario.html", time=f"{now.hour}:{now.minute}:{now.second}", color="blue")
+      
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
